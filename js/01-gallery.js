61dcf159a01b6f2d169d547ff7a296fc2c996b8f
Karol Trombala <ktrombala@gmail.com>
2023-06-06 18:08:11 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+for (const item of galleryItems) {
+    galleryEl.insertAdjacentHTML("beforeend", 
+    `<div class="gallery_item">
+    <a class="gallery_link" href="${item.original}">
+    <img 
+    class="gallery_image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>
+    </div`);
+}
