92afabb377ea7bba53be9989d90b5cf7a4302449
Karol Trombala <ktrombala@gmail.com>
2023-06-14 21:00:45 +0200

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,26 +5,6 @@ console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
 
-// OLD VERSION
-
-// for (const item of galleryItems) {
-//   galleryEl.insertAdjacentHTML(
-//     "beforeend",
-//     `<div class="gallery__item">
-//     <a class="gallery__link" href="${item.original}">
-//     <img
-//     class="gallery__image"
-//     src="${item.preview}"
-//     data-source="${item.original}"
-//     alt="${item.description}"
-//     />
-//     </a>
-//     </div`
-//   );
-// }
-
-// NEW VERSION
-
 const galleryMap = galleryItems
   .map((item) => {
     galleryEl.insertAdjacentHTML(
@@ -46,14 +26,17 @@ const galleryMap = galleryItems
 galleryEl.addEventListener("click", (e) => {
   e.preventDefault();
   const instance = basicLightbox.create(
-    `<img src="${e.target.dataset.source}">`
-  );
-
-  instance.show();
-
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      instance.close();
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onClose: (instance) => {
+        document.addEventListener("keydown", (e) => {
+          if (e.key === "Escape")
+            instance.close(() =>
+              document.removeEventListener("keydown", "Escape")
+            );
+        });
+      },
     }
-  });
+  );
+  instance.show();
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,25 +5,6 @@ console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
 
-// OLD VERSION
-
-// for (const item of galleryItems) {
-//   galleryEl.insertAdjacentHTML(
-//     "beforeend",
-//     `<div class="gallery__item">
-//     <a class="gallery__link" href="${item.original}">
-//     <img
-//     class="gallery__image"
-//     src="${item.preview}"
-//     alt="${item.description}"
-//     />
-//     </a>
-//     </div>`
-//   );
-// }
-
-// NEW VERSION
-
 const galleryMap = galleryItems
   .map((item) => {
     galleryEl.insertAdjacentHTML(
